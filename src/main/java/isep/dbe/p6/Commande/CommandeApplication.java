34c99f1be9e6538014288a998ecc5973dbfdf920
maremadev <maremefalilou979@gmail.com>
2026-07-03 21:12:05 -0400

    premier commit

---
+++ src/main/java/isep/dbe/p6/Commande/CommandeApplication.java
@@ -0,0 +1,13 @@
+package isep.dbe.p6.Commande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isep/dbe/p6/Commande/CommandeApplicationTests.java
@@ -0,0 +1,13 @@
+package isep.dbe.p6.Commande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
