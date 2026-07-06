93ffe5a2d08f0ed5386b8ecbb75dc8164d3f5b72
maremadev <maremefalilou979@gmail.com>
2026-07-05 22:43:19 -0400

    correction de la structure des exceptions

--- src/main/java/com/polytech/commandes/exception/RegleMetierException.java
+++
@@ -1,8 +0,0 @@
-package com.polytech.commandes.exception;
-
-public class RegleMetierException extends RuntimeException {
-
-    public RegleMetierException(String message) {
-        super(message);
-    }
-}
